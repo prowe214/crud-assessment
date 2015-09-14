81fe616def29a43e8b3fec78ae6b9185b4d0a614
Paul Cullen Rowe <prowe214@gmail.com>
2015-09-14 13:52:44 -0600

    styles

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 var db = require('monk')('localhost/assessments');
 var jobs = db.get('jobs');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  jobs.find({}, function (err, docs) {
+  jobs.find({$query: {}, $orderby: {date: -1}}, function (err, docs) {
     res.render('index', {jobs: docs});
   });
 });
@@ -16,7 +16,7 @@ router.post('/', function(req, res, next) {
     company: req.body.company,
     shortDescription: req.body.shortDescription,
     details: req.body.details,
-    date: Date.now(),
+    date: moment().format('l'),
     open: true,
     applicants: []
   });
@@ -33,9 +33,11 @@ router.get('/:id', function (req, res, next) {
   });
 });
 
-router.post('/:id', function (req, res, next) {
+router.post('/:id/apply', function (req, res, next) {
+  console.log(req.body);
   jobs.update({_id: req.params.id}, {$push: {
     applicants: {
+      applicantId: Math.floor(Math.random() * 10000000),
       name: req.body.name,
       email: req.body.email,
       resume: req.body.resume
@@ -51,13 +53,23 @@ router.get('/:id/edit', function (req, res, next) {
 });
 
 router.post('/:id/edit', function(req, res, next) {
+  if (req.body.open === "true") req.body.open = true;
+  if (req.body.open === "false") req.body.open = false;
   jobs.update({_id: req.params.id}, { $set: {
     title: req.body.title,
     company: req.body.company,
     shortDescription: req.body.shortDescription,
-    details: req.body.details
+    details: req.body.details,
+    open: req.body.open
   }});
   res.redirect('/' + req.params.id);
+});
+
+router.get('/jobs/:id/delete/:applicantId', function (req, res, next) {
+  jobs.update({}, {$pull: {
+    applicants: { _id: req.params.applicantId }
+  }});
+  res.redirect('/');
 });
 
 router.get('/:id/delete', function (req, res, next) {
